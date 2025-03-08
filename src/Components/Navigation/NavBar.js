102c00596c897309581c9b4a9c264fce8b9b2971
Udara Dilshan <ict21013@std.uwu.ac.lk>
2025-03-08 11:30:49 +0530

    add logo

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function NavBar() {
   return (
     <div className="NavBarBox">
-        <div className="navBarLeft"> </div>
+        <div className="navBarLeft">
+            <span className="logo">FaceBook</span>
+        </div>
         <div className="navBarCenter"></div>
         <div className="navBarRight"></div>
     </div>
